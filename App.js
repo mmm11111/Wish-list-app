8da53abde6233ac07fd6b40beb52837c80c77b42
Ephrem <eengida@ggc.edu>
2022-02-10 23:31:59 -0500

    basic frontEnd set up, added some pics

--- App.js
+++ App.js
@@ -1,10 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,ImageBackground,Pressable,Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ImageBackground 
+      source={require('./assets/shoping.jpg')}
+      
+      style = {styles.pic}
+      />
+      <View>
+      <Text style = {styles.title}>Wish-list App</Text>
+      </View>
+      <View style ={styles.loginContainer}>
+        <Pressable 
+        style = {styles.login} 
+        onPress = {() => {
+          console.warn("logging in")   //just for now
+        }}
+        
+        >
+          <Text style = {styles.loginText}>Login</Text>
+          
+          
+        </Pressable>
+        </View>
+
+        <View style = {styles.loginContainer}>
+                
+        <Pressable  
+        style = {styles.login} 
+        onPress = {() => {
+          console.warn("logging in")   
+        }}
+        
+        >
+          <Text style = {styles.loginText}>Signup</Text>
+          
+          
+        </Pressable>
+      </View>
+     
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +53,46 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  pic:{
+    width: "80%",
+    height:"95%",
+    resizeMode: "cover",
+    position:'absolute'
+
+
+  },
+  title:{
+    marginTop: '15%',
+    
+    alignItems:'center',
+    width:'100%',
+    fontWeight: '500',
+    fontSize:46,
+    
+    
+  },
+  loginContainer:{
+    width:'100%',
+    padding:10
+
+
+  },
+  login:{                                                            //button
+    backgroundColor:"blue",
+    height:40,
+    borderRadius:20,
+    justifyContent:"center",
+    alignItems:'center',
+   
+    
+
+  },
+  loginText:{
+    fontSize:12,
+    fontWeight:'500',
+    textTransform:'uppercase',
+
+  }
+
+  
 });
